241f67324957a0cc2a220b763f3fc923a98ec9cf
Fan Bo <bo_fan@qq.com>
2026-04-10 11:31:47 +0300

    update README file

--- python_met_radar/setup.py
+++ python_met_radar/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='met_radar',
-    version='0.1',
+    version='1.1',
     packages=find_packages(),
     install_requires=[
         # 'numpy',
